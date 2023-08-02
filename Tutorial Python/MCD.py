5b82b782d30705c057129384a89fc3d1cf14324e
Alberto Muñoz Martínez <a.a.munoz.martinez@avanade.com>
2023-08-02 08:47:10 +0200

    first commit

---
+++ Tutorial Python/MCD.py
@@ -0,0 +1,14 @@
+def calcular_mcd(a, b):
+    while b != 0:
+        a, b = b, a % b
+    return a
+
+# Solicitamos los números al usuario
+numero1 = int(input("Ingresa el primer número: "))
+numero2 = int(input("Ingresa el segundo número: "))
+
+# Calculamos el MCD usando la función que definimos
+mcd = calcular_mcd(numero1, numero2)
+
+# Mostramos el resultado
+print(f"El MCD de {numero1} y {numero2} es: {mcd}")
